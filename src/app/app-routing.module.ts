8e4d57dd9d57b96318b04f2c749072064385a69d
Ray Huapaya <1567543@uab.cat>
2022-11-02 12:15:03 +0100

    practica3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroeComponent } from './comunes/heroe/heroe.component';
+import { HeroesComponent } from './comunes/heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: HeroesComponent},
+  {path:"heroes", component: HeroesComponent},
+  {path: 'heroe/:id', component: HeroeComponent},
+  {path:"**", component: HeroesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './comunes/header/header.component';
+import { HeroesComponent } from './comunes/heroes/heroes.component';
+import { HeroeComponent } from './comunes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
